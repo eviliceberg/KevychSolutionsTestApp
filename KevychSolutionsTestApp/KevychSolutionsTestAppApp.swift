245612b44367cf6a4eb13622f7a4e908cd371105
evilicebergs <voiced.fair-0a@icloud.com>
2025-06-20 01:29:52 +0300

    Initial Commit

---
+++ KevychSolutionsTestApp/KevychSolutionsTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KevychSolutionsTestAppApp.swift
+//  KevychSolutionsTestApp
+//
+//  Created by Artem Golovchenko on 20.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct KevychSolutionsTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
